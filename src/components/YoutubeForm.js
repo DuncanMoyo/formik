3974c05db05e021a1ec423690ff8d2b11bbe6f08
Duncan Moyo <duncanfmoyo@gmail.com>
2022-10-09 22:03:29 +0200

    t-8 displaying error messages

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -2,72 +2,78 @@ import React from "react";
 import { useFormik } from "formik";
 
 const initialValues = {
-	name: "",
-	email: "",
-	channel: "",
-}
+  name: "",
+  email: "",
+  channel: "",
+};
 
-const onSubmit = values => {
-	console.log("Form Data", values);
-}  
+const onSubmit = (values) => {
+  // console.log("Form Data", values);
+};
 
-const validate =  values => {
-	let errors = {}
-	if(!values.name) {
-		errors.name = 'Required'
-	}
-	if(!values.email) {
-		errors.email = 'Required'
-	} else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-		errors.email = 'Invalid e-mail Format'
-	}
-	if(!values.channel) {
-		errors.channel = 'Required'
-	}
-	return errors
-}
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = "Required";
+  }
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid e-mail Format";
+  }
+  if (!values.channel) {
+    errors.channel = "Required";
+  }
+  return errors;
+};
 
 const YoutubeForm = () => {
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate
+    validate,
   });
 
-  const {handleSubmit, values, handleChange} = formik;
+  const { handleSubmit, values, handleChange, errors } = formik;
 
-//   console.log("Form Values", values);
+  console.log("Form Errors", errors);
 
   return (
     <div>
-      <form onSubmit={handleSubmit} >
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          onChange={handleChange}
-          value={values.name}
-        />
-
-        <label htmlFor="email">E-mail</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          onChange={handleChange}
-          value={values.email}
-        />
-
-        <label htmlFor="channel">Channel</label>
-        <input
-          type="text"
-          id="channel"
-          name="channel"
-          onChange={handleChange}
-          value={values.channel}
-        />
-
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={handleChange}
+            value={values.name}
+          />
+          {errors.name ? <div className="error" >{errors.name}</div> : null}
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={handleChange}
+            value={values.email}
+          />
+          {errors.email ? <div className="error">{errors.email}</div> : null}
+        </div>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            onChange={handleChange}
+            value={values.channel}
+          />
+          {errors.channel ? <div className="error">{errors.channel}</div> : null}
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
