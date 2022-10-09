65b42b05924e6463a5de3b7ba6781883314eb7aa
Duncan Moyo <duncanfmoyo@gmail.com>
2022-10-09 19:12:20 +0200

    t-3 useFormik Hook

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import {useFormik} from 'formik'
 
 const YoutubeForm = () => {
+
+    const formik = useFormik({
+        
+    })
+
   return (
     <div>
         <form>
