f4455c6d5f645952986f376e15f80e6221f74590
Duncan Moyo <duncanfmoyo@gmail.com>
2022-10-09 20:03:22 +0200

    t-7 validation function

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,16 +1,37 @@
 import React from "react";
 import { useFormik } from "formik";
 
+const initialValues = {
+	name: "",
+	email: "",
+	channel: "",
+}
+
+const onSubmit = values => {
+	console.log("Form Data", values);
+}  
+
+const validate =  values => {
+	let errors = {}
+	if(!values.name) {
+		errors.name = 'Required'
+	}
+	if(!values.email) {
+		errors.email = 'Required'
+	} else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+		errors.email = 'Invalid e-mail Format'
+	}
+	if(!values.channel) {
+		errors.channel = 'Required'
+	}
+	return errors
+}
+
 const YoutubeForm = () => {
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: "",
-    },
-    onSubmit: values => {
-        console.log("Form Data", values);
-    }  
+    initialValues,
+    onSubmit,
+    validate
   });
 
   const {handleSubmit, values, handleChange} = formik;
