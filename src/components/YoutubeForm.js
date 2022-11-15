8278e12cdb7cab76eb098dffaf5edd505299c714
Duncan Moyo <duncanfmoyo@gmail.com>
2022-11-15 11:07:25 +0200

    t-22 FieldArray Component

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
 import * as Yup from "yup";
 import TextError from "./TextError";
 
@@ -10,10 +10,11 @@ const initialValues = {
   comments: "",
   address: "",
   social: {
-    facebook: '',
-    twitter: ''
+    facebook: "",
+    twitter: "",
   },
-  phoneNumbers: ['', '']
+  phoneNumbers: ["", ""],
+  phNumbers: [""],
 };
 
 const onSubmit = (values) => {
@@ -45,9 +46,7 @@ const YoutubeForm = () => {
           <label htmlFor="email">E-mail</label>
           <Field type="email" id="email" name="email" />
           <ErrorMessage name="email">
-            {
-              errorMsg => <div className="error" >{errorMsg}</div>
-            }
+            {(errorMsg) => <div className="error">{errorMsg}</div>}
           </ErrorMessage>
         </div>
         <div className="form-control">
@@ -65,11 +64,11 @@ const YoutubeForm = () => {
           <label htmlFor="address">Address</label>
           <Field name="address">
             {(props) => {
-              const { field, form, meta } = props;
+              const { field, meta } = props;
               console.log("Render Props: ", props);
               return (
                 <div>
-                  <input type='text' id="address" {...field} />
+                  <input type="text" id="address" {...field} />
                   {meta.touched && meta.error ? <div>{meta.error}</div> : null}
                 </div>
               );
@@ -91,6 +90,35 @@ const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="secondaryPh">Secondary Phone Number</label>
           <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="phNumbers">List of Phone Numbers</label>
+          <FieldArray name="phNumbers">
+            {(fieldArrayProps) => {
+              console.log("fieldArrayProps", fieldArrayProps);
+              const { push, remove, form } = fieldArrayProps;
+              const { values } = form;
+              const { phNumbers } = values;
+              return (
+                <div>
+                  {phNumbers.map((phNumber, index) => (
+                    <div key={index}>
+                      <Field name={`phNumbers[${index}]`} />
+                      {index > 0 && (
+                        <button type="button" onClick={() => remove(index)}>
+                          -
+                        </button>
+                      )}
+
+                      <button type="button" onClick={() => push("")}>
+                        +
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              );
+            }}
+          </FieldArray>
         </div>
         <button type="submit">Submit</button>
       </Form>
